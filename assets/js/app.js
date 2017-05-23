5b06c8385691b6363a4ee90933b69edf954739c3
itsandromeda <itsandromeda@gmail.com>
2017-05-22 22:21:33 -0500

    Extras

--- assets/js/app.js
+++ assets/js/app.js
@@ -19,3 +19,18 @@ document.getElementById("close").addEventListener('click', function (e) {
     section5.style.top = "0";
     footer.style.top = "0";
 });
+
+function display() {
+    "use strict";
+    var x = document.getElementById('myDIV');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
+
+document.getElementById("extra-1").addEventListener('click' function () {
+   "use strict";
+    
+});
